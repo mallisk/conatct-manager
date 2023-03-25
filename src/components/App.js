b66842cc76f85120d238baaccf542f9a2fa71ab8
mallisk <malli.sk@yahoo.com>
2023-03-25 13:20:43 +0530

    initial version of conatct manager

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import AddContactList from "./AddContactList";
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    
+    <Header></Header>
+    // <AddContactList></AddContactList>
+    // <ContactList></ContactList>
+  );
+}
+
+export default App;
